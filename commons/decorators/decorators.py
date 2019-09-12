5818208c3db61bb11c4f3b5d38bfbad924baf0c6
Agustin Rojas <your_email@abc.com>
2019-09-12 00:09:14 -0300

    Fix from data owner decorator

--- commons/decorators/decorators.py
+++ commons/decorators/decorators.py
@@ -1,3 +1,6 @@
+from commons.operations_utils.functions import deserialize, serialize
+
+
 def optimized_collection_parameter(optimization, active=False):
     def wrap(f):
         def wrapped_optimized_collection_parameter(*args):
@@ -7,6 +10,7 @@ def optimized_collection_parameter(optimization, active=False):
             return f(*params)
         return wrapped_optimized_collection_parameter
     return wrap
+
 
 def optimized_collection_response(optimization, active=False):
     def wrap(f):
@@ -28,6 +32,7 @@ def optimized_dict_collection_response(optimization, active=False):
         return wrapped_optimized_collection_response
     return wrap
 
+
 def normalize_optimized_collection_argument(active=False):
     def wrap(f):
         def wrapped_normalize_optimized_collection(*args):
@@ -39,6 +44,7 @@ def normalize_optimized_collection_argument(active=False):
         return wrapped_normalize_optimized_collection
     return wrap
 
+
 def normalize_optimized_response(active=True):
     def wrap(f):
         def wrapped_normalize_optimized_response(*args):
@@ -46,4 +52,19 @@ def normalize_optimized_response(active=True):
             result['model']['weights'] = result['model']['weights'].tolist() if active else result['model']['weights']
             return result
         return wrapped_normalize_optimized_response
+    return wrap
+
+
+def data_owner_computation():
+    def wrap(f):
+        def wrapped_data_owner_computation(*args):
+            encryption_service = args[0].encryption_service
+            collection = args[2]
+            public_key = args[3]
+            collection = deserialize(collection, encryption_service, public_key)
+            params = list(args)
+            params[2] = collection
+            result = f(*params)
+            return serialize(result, encryption_service, public_key)
+        return wrapped_data_owner_computation
     return wrap
