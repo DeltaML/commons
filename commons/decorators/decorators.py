b766566033918412e08a78ce6f0dcd24bcbe489a
Agustin Rojas <your_email@abc.com>
2019-09-11 23:59:57 -0300

    Fix changes from federated aggregator

--- commons/decorators/decorators.py
+++ commons/decorators/decorators.py
@@ -1,11 +1,12 @@
 def optimized_collection_parameter(optimization, active=False):
     def wrap(f):
         def wrapped_optimized_collection_parameter(*args):
-            params = args[0], optimization(args[1]) if active else args[1]
+            collection = optimization(args[2]) if active else args[2]
+            params = list(args)
+            params[2] = collection
             return f(*params)
         return wrapped_optimized_collection_parameter
     return wrap
-
 
 def optimized_collection_response(optimization, active=False):
     def wrap(f):
@@ -20,8 +21,8 @@ def optimized_dict_collection_response(optimization, active=False):
     def wrap(f):
         def wrapped_optimized_collection_response(*args):
             result = f(*args)
-            updates = list(map(lambda x: x['update']['weights'], result))
-            owners = list(map(lambda x: x['data_owner'], result))  # TODO: Add model_id
+            updates = list(map(lambda x: x['update'], result))
+            owners = list(map(lambda x: x['data_owner_id'], result))  # TODO: Add model_id
             updates = optimization(updates) if active else updates
             return updates, owners
         return wrapped_optimized_collection_response
@@ -31,17 +32,18 @@ def normalize_optimized_collection_argument(active=False):
     def wrap(f):
         def wrapped_normalize_optimized_collection(*args):
             # TODO: Refactor!!!
-            params = args[0], args[1], args[2].tolist() if active else args[2]
+            data = args[2].tolist() if active else args[2]
+            params = list(args)
+            params[2] = data
             return f(*params)
         return wrapped_normalize_optimized_collection
     return wrap
 
-
-def normalize_optimized_response(active=False):
+def normalize_optimized_response(active=True):
     def wrap(f):
         def wrapped_normalize_optimized_response(*args):
             result = f(*args)
-            result['model'] = result['model'].tolist() if active else result['model']
+            result['model']['weights'] = result['model']['weights'].tolist() if active else result['model']['weights']
             return result
         return wrapped_normalize_optimized_response
     return wrap
